d1cde95274b9b65c9cd6d5c4c83f0ea59fc35d96
Ian Felton <ian.felton@bestbuy.com>
2015-11-06 13:20:39 -0600

    Handle errors

--- spec/availability.spec.js
+++ spec/availability.spec.js
@@ -11,7 +11,6 @@ bby.init({
 describe('The Availability section of the BBY API', function() {
     describe('Fetch availability', function() {
         it('LIVE: Availability search', function(done) {
-
             bby.stores('area(55119,25)&storeType=BigBox')
                 .then(function(data) {
                     expect(data.stores.length > 0).toBe(true);
@@ -41,6 +40,44 @@ describe('The Availability section of the BBY API', function() {
                     done();
                 });
             });
+        });
+
+        it('LIVE: Availability search using callback error', function(done) {
+            bby.availability(1780275, 'blah', function(err, data) {
+                expect(err).not.toBe(null);
+                expect(err).toBe("Second parameter of \"availability\" must be store id(s), and it must be either a number or array of numbers");
+                done();
+            });
+        });
+
+        it('LIVE: Availability search using promises error', function(done) {
+            bby.availability(1780275, 'blah')
+                .then(function(data) {
+                    //expect(data).toBe(null);
+                })
+                .catch(function(err) {
+                    expect(err).not.toBeUndefined();
+                    expect(err).toBe("Second parameter of \"availability\" must be store id(s), and it must be either a number or array of numbers");
+                })
+                .finally(done);
+        });
+        it('LIVE: Availability search using promises sku error', function(done) {
+            bby.availability({}, 'blah')
+                .then(function(data) {})
+                .catch(function(err) {
+                    expect(err).not.toBeUndefined();
+                    expect(err).toBe("First parameter of \"availability\" must be the SKU, and it must be either a number or a string");
+                })
+                .finally(done);
+        });
+        it('LIVE: Availability search using promises too many parameters error', function(done) {
+            bby.availability(1780275, 123, 123, 123)
+                .then(function(data) {})
+                .catch(function(err) {
+                    expect(err).not.toBeUndefined();
+                    expect(err).toBe("Unrecognized parameter length when calling \"availability\" method");
+                })
+                .finally(done);
         });
     });
 });
